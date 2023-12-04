741da00c035ccebdd4e2b60317e1199efc43db75
destny69 <mzldestiny69@gmail.com>
2023-12-04 12:55:27 +0545

    Day 1 completed

--- array_implementation_of_list.cpp
+++ array_implementation_of_list.cpp
@@ -1,56 +1,130 @@
 
-// Implementing a List using an array in C++
-
-
-
-
 #include<iostream>
 
 using namespace std;
 
 class List {
 
-private: //Declearing private data variable
+private:
     int data[100];
     int position;
 
 public:
-    //Constructor
-    List () {
-        position = -1;
+    List() {
+        position = 0;
     };
 
-   void create(int val); //Add data to the list linearly
-   void insert_data(int pos, int val); // Add data to the list at specified position
-   void delete_data(int pos); // Delete data from the specified position of list
-   void modify(int pos, int val); // Modification of data from desired position
-   void transverse(); // Display all element of list
-   void Merging(List l1, List L2); //merge two existing list
+    int getsize() {
+        return position;
+    };
 
+    void create(int val);
+    void insert_data(int pos, int val);
+    void delete_at_position(int pos);
+    void modify(int pos, int val);
+    void transverse();
+    List Merging(List l1, List l2);
 };
 
-
-void List :: create(int val)
-{
+void List::create(int val) {
     data[position++] = val;
-    cout << position;
 }
 
-void List :: insert_data (int pos, int val)
-{
-    for(int i = position; i > pos ; i-- )
-    {
-        data[i+1] = data[i];
-
-
+void List::insert_data(int pos, int val) {
+    int i;
+    if (pos < position) {
+        for (i = position; i > pos; i--) {
+            data[i] = data[i - 1];
+        }
+        data[i] = val;
+        position++;
+    }
+    else if (pos == position) {
+        data[position++] = val;
+    }
+    else {
+        cout << "\nIndex out of range. Data not inserted.\n";
     }
 }
 
+void List::delete_at_position(int pos) {
+    if (pos < position) {
+        for (int i = pos; i < position - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        position--;
+    }
+    else {
+        cout << "\nIndex out of range. Data not deleted.\n";
+    }
+}
 
-int main()
-{
+void List::modify(int pos, int val) {
+    if (pos < position) {
+        data[pos] = val;
+    }
+    else {
+        cout << "\nIndex out of range. Data not modified.\n";
+    }
+}
 
-    List l1;
+void List::transverse() {
+    for (int i = 0; i < position; i++) {
+        cout << data[i] << " ";
+    }
+    cout << endl;
+}
+List List::Merging(List l1, List l2) {
+    List l3;
+    int pos = 0;
+
+    cout << "\nList 1 data:\n";
+    l1.transverse();
+    cout << "\nList 2 data:\n";
+    l2.transverse();
+
+    for (pos = 0; pos < l1.position; pos++) {
+        l3.data[l3.position++] = l1.data[pos];
+    }
+
+    for (int i = 0; i < l2.position; i++) {
+        l3.data[l3.position++] = l2.data[i];
+    }
+
+
+    return l3;
+}
+
+
+int main() {
+
+    List l1, l2, l3;
     l1.create(5);
+    l1.create(4);
+    l1.create(3);
+    l1.create(2);
+    l1.insert_data(2, 7); // Inserting at a random index
+    l1.insert_data(10, 9); // Inserting at an index out of range
+    cout << "\nAfter Insertion:\n";
+    l1.transverse();
+    l1.delete_at_position(9);
+    cout << "\nAfter deletion:\n";
+    l1.transverse();
+    l1.delete_at_position(100);
+    l1.modify(4, 20);
+    cout << "\nAfter modification:\n";
+    l1.transverse();
+
+    l2.create(10);
+    l2.create(14);
+    l2.create(13);
+    l2.create(12);
+    cout << "\nElements of list 2:\n";
+    l2.transverse();
+
+    l3 = l3.Merging(l1, l2);
+    cout << "\nAfter merging:\n";
+    l3.transverse();
+
     return 0;
 }
